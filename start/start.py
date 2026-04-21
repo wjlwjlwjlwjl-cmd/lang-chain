7a7e101952c2d7de43c990d878fc6d5d50c051aa
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-04-21 18:41:26 +0800

    continue...

---
+++ start/start.py
@@ -0,0 +1,18 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.runnables import RunnableSequence
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+
+model = ChatOpenAI(
+    model="qwen-turbo",
+)
+messages = [
+    SystemMessage("Translate the sentence from English to Chinese"),
+    HumanMessage("who are you!")
+]
+parser = StrOutputParser()
+#chain = model | parser
+#chain = RunnableSequence(first = model, last = parser)
+chain = model.pipe(parser)
+result = chain.invoke(messages)
+print(result)
